7aee37df262183b0e79d0f9edb35423977c60c1f
k3b <1374583+k3b@users.noreply.github.com>
2021-05-29 08:48:42 +0200

    removed ndk from main app

--- app/src/main/cpp/native-lib.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <string>
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_org_vosk_HelloNative_stringFromJNI(JNIEnv *env, jobject thiz) {
-    std::string hello = "Hello from lib C++";
-    return env->NewStringUTF(hello.c_str());
-}
